c7091305ca4c434b853e1a013d1b589a969b2044
repl.it user <replituser@example.com>
2020-07-31 17:20:20 +0000

    fixed merge conflicts in models

--- ahia/models.py
+++ ahia/models.py
@@ -52,21 +52,16 @@ class Asset(models.Model):
         return self.title
 
 #Model for Posting in timeline so people can see your post
-<<<<<<< HEAD
-# class Post(models.Model):
-# 	post_title = models.CharField(max_length=100)
-# 	post_asset = models.ForeignKey(Asset, null=True,blank=True,upload_to='Files')
-# 	Post_content = models.TextField()
-# 	post_date_posted = models.DateTimeField(default=timezone.now)
-# 	post_author = models.ForeignKey(User, on_delete=models.CASCADE)
-=======
+
+ 
+
 class Post(models.Model):
 	post_title = models.CharField(max_length=100)
 	post_asset = models.ForeignKey(Asset, null=True,blank=True, on_delete=models.CASCADE)
 	Post_content = models.TextField()
 	post_date_posted = models.DateTimeField(default=timezone.now)
 	post_author = models.ForeignKey(User, on_delete=models.CASCADE)
->>>>>>> master
+
 
 # 	def __str__(self):
 # 		return self.title
@@ -80,40 +75,26 @@ class Post(models.Model):
 
 
 
-<<<<<<< HEAD
-# #Chat model for messaging if needed 
-# class Chat(models.Models):
-#   chat_sender = models.ForeignKey(User, related_name="sender")
-#   chat_receiver = models.ForeignKey(User, related_name="receiver")
-#   chat_msg = models.TextField(max_length=1000)
-#   chat_timestamp = models.DateTimeField(auto_now_add=True)
-=======
+
 #Chat model for messaging if needed 
 class Chat(models.Model):
   chat_sender = models.ForeignKey(User, related_name="sender", on_delete=models.CASCADE)
   chat_receiver = models.ForeignKey(User, related_name="receiver", on_delete=models.CASCADE)
   chat_msg = models.TextField(max_length=1000)
   chat_timestamp = models.DateTimeField(auto_now_add=True)
->>>>>>> master
+
 
 #   def __str__(self):
 #       return str(self.id) + ": from " + str(self.sender) + " to " + str(self.receiver)
       
-<<<<<<< HEAD
-# #Comment model for commenting on post 
-# class Comment(models.Model):
-#     com_body = models.TextField()
-#     com_pub_time = models.DateTimeField()
-#     com_user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     com_ebook = models.ForeignKey(Asset, on_delete=models.CASCADE)
-=======
+
 #Comment model for commenting on post 
 class Comment(models.Model):
     com_body = models.TextField()
     com_pub_time = models.DateTimeField()
     com_user = models.ForeignKey(User, on_delete=models.CASCADE)
     com_ebook = models.ForeignKey(Asset, on_delete=models.CASCADE)
->>>>>>> master
+
 
 
 #     def pub_date_pretty(self):
